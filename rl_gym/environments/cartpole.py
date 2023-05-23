6b0a0cd91bd89ca7d9a5dbe1b38f58061a6ff294
Silas Vance <phosubvieworsi@gmx.net>
2023-05-23 14:08:00 -0400

    ci: Setup basic CI/CD for RL experiments

--- rl_gym/environments/cartpole.py
+++ rl_gym/environments/cartpole.py
@@ -150,3 +150,4 @@ if __name__ == "__main__":
 # Simulated change on 2023-03-30 09:32:00
 # Simulated change on 2023-04-28 16:40:00
 # Simulated change on 2023-05-11 17:20:00
+# Simulated change on 2023-05-23 14:08:00
