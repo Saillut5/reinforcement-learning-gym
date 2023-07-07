1a23ef03562ce7790115c962dfb8fb8dfc1de521
Silas Vance <phosubvieworsi@gmx.net>
2023-07-07 12:59:00 -0400

    refactor: Modularize RL agents

--- rl_gym/environments/cartpole.py
+++ rl_gym/environments/cartpole.py
@@ -155,3 +155,4 @@ if __name__ == "__main__":
 # Simulated change on 2023-06-12 15:14:00
 # Simulated change on 2023-06-14 15:11:00
 # Simulated change on 2023-06-30 12:24:00
+# Simulated change on 2023-07-07 12:59:00
