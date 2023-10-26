2bea6f7bb8b67bdd505c975822499c26cc10f5d0
Silas Vance <phosubvieworsi@gmx.net>
2023-10-26 14:02:00 -0400

    feat: Integrate custom reward functions

--- rl_gym/environments/cartpole.py
+++ rl_gym/environments/cartpole.py
@@ -166,3 +166,4 @@ if __name__ == "__main__":
 # Simulated change on 2023-09-29 12:39:00
 # Simulated change on 2023-10-06 16:49:00
 # Simulated change on 2023-10-24 16:53:00
+# Simulated change on 2023-10-26 14:02:00
